cbf33d8da1991737ea45cfc71489547caba0d621
emoriyukino <yukino.e.1994@gmail.com>
2019-12-02 11:41:26 +0900

    変数定数　課題１課題３　再提出

--- Valiable/src/kadai1.java
+++ Valiable/src/kadai1.java
@@ -2,9 +2,10 @@
 public class kadai1 {
 
 	public static void main(String[] args) {
+		//小数点表示
 		double num = 31.2;
 		System.out.println(num);
-		
+	    //文字列
 		String name = "Java研修";
 		System.out.println(name);
 		
@@ -16,7 +17,7 @@ public class kadai1 {
         System.out.println(number2);
 		
 		String name1="才";
-		 System.out.println(name1);
+		System.out.println(name1);
 
 				
 		
--- Valiable/src/kadai3.java
+++ Valiable/src/kadai3.java
@@ -2,36 +2,35 @@
 public class kadai3 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-       
-		System.out.println("変換前");	
 		
-		int intNum = 219;
+
+	   System.out.println("変換前");
+
+	   int intNum = 219;
        System.out.println(intNum);
-       
+
        double doubleNum = 364.2;
        System.out.println(doubleNum);
-       
+
        long longNum = 123456l;
        System.out.println(longNum);
-       
+
        System.out.println("変換後");
-       
-       
-       long intNum2= (long)219L;
-       System.out.println(intNum2);
-       
-       float doubleNum2 = (float)364.2;
-       System.out.println(doubleNum2);
-       
-       double longNum2 = (long)123456;
-       System.out.println(longNum2);
-       
-       
-       
-       
-      
-       
+
+       long longNumchange = (long)intNum;
+       System.out.println(longNumchange);
+
+       float floatNum = (float)doubleNum;
+       System.out.println(floatNum);
+
+       double doubleNumchange = (long)longNum;
+       System.out.println(doubleNumchange);
+
+
+
+
+
+
 	}
 
 }
