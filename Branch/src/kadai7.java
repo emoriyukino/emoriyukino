9e35a7b2b309049554e86949ccbb0bc168cf8c14
emoriyukino <yukino.e.1994@gmail.com>
2019-12-08 17:21:01 +0900

    条件分岐　課題１、４、５の再提出

--- Branch/src/kadai7.java
+++ Branch/src/kadai7.java
@@ -3,28 +3,21 @@ import java.util.Scanner;
 public class kadai7 {
 
 	public static void main(String[] args) {
-		Scanner sc = new java.util.Scanner(System.in);
-		String msgA = sc.nextLine();
 		System.out.println("病院に行く曜日を入力してください");
 		System.out.println("【日曜,月曜,火曜,水曜,木曜,金曜,土曜】");
+		Scanner sc = new java.util.Scanner(System.in);
+		String msgA = sc.nextLine();
 		switch(msgA) {
 		 case "月曜":
-			 System.out.println("午前診療と午後診療があります");
-			 break;
 		 case "火曜":
-			 System.out.println("午前診療と午後診療があります");
-			 break;
 		 case "水曜":
-			 System.out.println("午前診療と午後診療があります");
-			 break;
 		 case "木曜":
-			 System.out.println("午前診療と午後診療があります");
-			 break;
 		 case "金曜":
 			 System.out.println("午前診療と午後診療があります");
 			 break;
 		 case "土曜":
 			 System.out.println("土曜は午前診療のみです");
+			 break;
 		 case "日曜":
 			 System.out.println("日曜は休診です");
 			 break;
