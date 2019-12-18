1fd693822b73a8d7e43b93082a221d92db6e0265
emoriyukino <yukino.e.1994@gmail.com>
2019-12-18 20:47:53 +0900

    メソッド　課題３と５　

--- Method/src/kadai3.java
+++ Method/src/kadai3.java
@@ -1,11 +1,9 @@
 
 public class kadai3 {
-
 	public static void main(String[] args) {
 		System.out.println("三角形の面積");
 		double areaResult = triarea(11.5, 23.5);
 		System.out.println(areaResult);
-		
 	}
 	public static double triarea(double bottom, double height) {
 		double area = height * bottom / 2;
--- Method/src/kadai5.java
+++ Method/src/kadai5.java
@@ -1,37 +1,38 @@
-import java.util.Scanner;
 
-//import com.sun.tools.javac.util.StringUtils;
-
-
+import java.util.ArrayList;
 public class kadai5 {
-	public static void main(String[] args)throws Exception {
-		Scanner sc = new java.util.Scanner(System.in);
-		String[]Arrays = new String [4];
-		for(int b = 0; b < 4 ; b++) {
-			String msg = sc.nextLine();
-	        Arrays[b] = msg;
-				
-		}
+	public static void main(String[] args) {
+
+
+		ArrayList<String>Arrays = new ArrayList<>();
+		Arrays.add("吉田");
+		Arrays.add("井上");
+		Arrays.add("山崎");
+		ArrayList<String>Arrays2 = new ArrayList<>();
+		Arrays2.add("吉田");
+		Arrays2.add("井上");
+		Arrays2.add(null);
 		boolean Result = nullCheak(Arrays);
-		if(Result == true) {
-			System.out.println("---配列にnullがある場合----");
-			System.out.println(Result);
-		}else {
-			System.out.println("---配列にnullがない場合----");
-			System.out.println(Result);
-		}
-		sc.close();
+		boolean Result2 = nullCheak(Arrays2);
+
+		System.out.println("---配列にnullがある場合----");
+		System.out.println(Result);
+
+		System.out.println("---配列にnullがない場合----");
+		System.out.println(Result2);
+
 	}
 
-	public static boolean nullCheak (String[]arrays) {
-		boolean Result=false;
-		for(int a = 0 ; a < arrays.length; a++) {
-			if(arrays[a]== null) {
-			 Result = true;
+	public static boolean nullCheak (ArrayList<String> arrays) {
+		boolean bbb = false;
+		for(int a = 0 ; a < arrays.size(); a++) {
+			if(arrays.get(a) == null) {
+			 bbb = true;
 			 break;
 			}
 		}
-		return Result;
+
+		return bbb;
 	}
 }
 
