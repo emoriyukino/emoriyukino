1942391079716df1c11cc5eaf6f422c250e3ed2a
emoriyukino <yukino.e.1994@gmail.com>
2019-12-21 15:37:16 +0900

    インスタンスとクラス　Personクラス　指摘箇所修正再提出

--- Instance/src/Person.java
+++ Instance/src/Person.java
@@ -5,11 +5,11 @@ public class Person {
 	String name = "";
 	//= 0;は課題３で追加
 	int age = 0;
-	
+
 	//課題３で作成及び使用
 	public void myProfile() {
-		System.out.println("私の名前は"+this.name+"です。");
-		System.out.println("年齢は"+this.age+"です。");
+		System.out.println("私の名前は" + this.name + "です。");
+		System.out.println("年齢は" + this.age + "です。");
 		System.out.println("よろしくお願いします。");
 	}
 }
