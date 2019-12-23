ae57596e2a2d10ee89f2a7a75e9f2f2647f0790b
emoriyukino <yukino.e.1994@gmail.com>
2019-12-23 15:41:54 +0900

    メソッド　課題２と４　再提出

--- Method/src/kadai2.java
+++ Method/src/kadai2.java
@@ -1,18 +1,12 @@
 
 public class kadai2 {
 	public static void main(String[] args) {
-		task2("コラボ");
-		task2(30);
-		task2(178.0);
+		task2("コラボ", 30, 178.0);
 		System.out.println("よろしくお願いします。");
 	}
-	public static void task2(String name) {
-		System.out.println("私の名前は"+name+"です。");
-	}
-	public static void task2(int intNum) {
-		System.out.println("年齢は"+intNum+"です。");
-	}
-	public static void task2(double doubleNum) {
-		System.out.println("身長は"+doubleNum+"です。");
+	public static void task2(String name, int intNum, double doubleNum) {
+		System.out.println("私の名前は"+ name + "です。");
+		System.out.println("年齢は"+ intNum + "です。");
+		System.out.println("身長は"+ doubleNum + "です。");
 	}
 }
--- Method/src/kadai4.java
+++ Method/src/kadai4.java
@@ -1,14 +1,5 @@
 
 public class kadai4 {
-	public static String overload(String a, String b) {
-		return a + b;
-	}
-	public static int overload(int a, int b) {
-		return a * b;
-	}
-	public static double overload(double a, double b) {
-		return a + b;
-	}
 	public static void main(String[] args) {
 		System.out.println("---①文字列の場合----");
 		System.out.println(overload("文字列１","文字列2"));
@@ -17,5 +8,15 @@ public class kadai4 {
 		System.out.println("---③少数の場合----");
 		System.out.println(overload(3.5, 5.5));
 	}
+	public static String overload(String a, String b) {
+		return a + b;
+	}
+	public static int overload(int c, int d) {
+		return c + d;
+	}
+	public static double overload(double e, double f) {
+		return e + f;
+	}
+
 
 }
